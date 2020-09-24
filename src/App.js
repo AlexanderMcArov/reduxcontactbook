91cd880cf998f0eade8997fae0f9845c7b39e6b6
Alexander McArov <alexandercom445@gmail.com>
2020-09-24 21:01:02 +0300

    redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './contactbook/contactbook.css'
 import ContactInput from './contactbook/ContactInput'
 import './App.css'
 
 function App() {
-  let [isLoad,setLoad] = useState(false)
+
   return (
     <div className="App">
       <header><div>Contact<span>Book</span></div></header>
